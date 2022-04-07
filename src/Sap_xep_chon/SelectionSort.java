39485a232d341f7cc4ae790e164b75b364210576
dunghojava <dungho21397@gmail.com>
2022-04-07 14:06:40 +0700

    sap xep chon

---
+++ src/Sap_xep_chon/SelectionSort.java
@@ -0,0 +1,33 @@
+package Sap_xep_chon;
+
+public class SelectionSort {
+    static double[] list = {1,9,4.5,6.6,5.7,-4.5};
+
+    public static void  selectionSort(double[] list) {
+        for (int i = 0; i < list.length-1; i++) {
+
+            double currrentMin = list[i];
+            int currentMinIndex = i;
+
+            for (int j = i+1; j < list.length; j++) {
+
+                if (currrentMin>list[j]) {
+                    currrentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currrentMin;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        selectionSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] +" ");
+        }
+    }
+}
